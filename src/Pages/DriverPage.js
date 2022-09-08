5a708cd45efa2110c60a69fc9d6c6375b4cba107
MarkBrianKariuki <kariukimarkbrian@gmail.com>
2022-09-09 02:05:13 +0300

    fetching driver data

--- src/Pages/DriverPage.js
+++ src/Pages/DriverPage.js
@@ -1,8 +1,47 @@
-import React from "react";
+import React, {useEffect, useState}from "react";
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button'
+import "../App.css"
+
 function DriverPage(){
+    const [drivers, setDriver] = useState([])
+    useEffect(()=> {
+        fetch("https://backendapi3.herokuapp.com/drivers")
+        .then((res)=>  res.json())
+        .then((data)=>{
+            console.log(data)
+            setDriver(data)
+
+        })
+    }, [])
+    function deleteDriver(id){
+        fetch(`https://backendapi3.herokuapp.com/drivers/${id}`,{
+            method: "DELETE",
+        })
+        .then(r => r.json())
+        .then(() => {
+            const deleting = drivers.filter((driver) => driver.id !== id)
+                setDriver(deleting)
+            })
+      }
     return(
         <div>
-            
+            {drivers.map((driver)=>{
+          return(<div>
+        <Card className="card" style={{ width: '18rem' }}>
+          <Card.Body>
+            <Card.Title>{driver.name}</Card.Title>
+            <Card.Text>{driver.licence_number}</Card.Text>
+          </Card.Body>
+          <Button onClick={() => {
+                  deleteDriver(driver.id) }}>Delete</Button>
+        </Card>
+          </div>
+
+          )
+        })}
+
+
         </div>
     )
 }
